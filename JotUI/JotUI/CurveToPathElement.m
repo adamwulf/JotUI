76be5d091df9622b45a3fd9007c069162118fb59
Adam Wulf <adam.wulf@gmail.com>
2015-05-20 22:58:06 -0500

    now dependent on the PerformanceBezier and ClippingBezier frameworks

--- JotUI/JotUI/CurveToPathElement.m
+++ JotUI/JotUI/CurveToPathElement.m
@@ -16,6 +16,7 @@
 #import "MoveToPathElement.h"
 #import "JotGLContext.h"
 #import "JotTrashManager.h"
+#import <ClippingBezier/ClippingBezier.h>
 
 #define kDivideStepBy 5
 #define kAbsoluteMinWidth 3.0
@@ -104,7 +105,7 @@ const CGPoint		JotCGNotFoundPoint = {-10000000.2,-999999.6};
     bez[2] = ctrl2;
     bez[3] = curveTo;
     
-    length = jotLengthOfBezier(bez, .1);
+    length = [UIBezierPath lengthOfBezier:bez withAccuracy:.1];
     return length;
 }
 
@@ -350,7 +351,7 @@ const CGPoint		JotCGNotFoundPoint = {-10000000.2,-999999.6};
         // leftover)
         CGFloat distToDot = realStepSize*step + (isFirstElementInStroke ? 0 : kBrushStepSize - previousElement.extraLengthWithoutDot);
 //        DebugLog(@" dot at %f", distToDot);
-        subdivideBezierAtLength(bez, leftBez, rightBez, distToDot, .1, subBezierlengthCache);
+        [UIBezierPath subdivideBezier:bez intoLeft:leftBez andRight:rightBez atLength:distToDot withAcceptableError:.1 withCache:subBezierlengthCache];
         CGPoint point = rightBez[0];
         
         GLfloat calcColor[4];
@@ -546,129 +547,6 @@ const CGPoint		JotCGNotFoundPoint = {-10000000.2,-999999.6};
 -(void) setColor:(UIColor *)_color{
     color = _color;
 }
-
-/**
- * will divide a bezier curve into two curves at time t
- * 0 <= t <= 1.0
- *
- * these two curves will exactly match the former single curve
- */
-static inline void subdivideBezierAtT(const CGPoint bez[4], CGPoint bez1[4], CGPoint bez2[4], CGFloat t){
-    CGPoint q;
-    CGFloat mt = 1 - t;
-    
-    bez1[0].x = bez[0].x;
-    bez1[0].y = bez[0].y;
-    bez2[3].x = bez[3].x;
-    bez2[3].y = bez[3].y;
-    
-    q.x = mt * bez[1].x + t * bez[2].x;
-    q.y = mt * bez[1].y + t * bez[2].y;
-    bez1[1].x = mt * bez[0].x + t * bez[1].x;
-    bez1[1].y = mt * bez[0].y + t * bez[1].y;
-    bez2[2].x = mt * bez[2].x + t * bez[3].x;
-    bez2[2].y = mt * bez[2].y + t * bez[3].y;
-    
-    bez1[2].x = mt * bez1[1].x + t * q.x;
-    bez1[2].y = mt * bez1[1].y + t * q.y;
-    bez2[1].x = mt * q.x + t * bez2[2].x;
-    bez2[1].y = mt * q.y + t * bez2[2].y;
-    
-    bez1[3].x = bez2[0].x = mt * bez1[2].x + t * bez2[1].x;
-    bez1[3].y = bez2[0].y = mt * bez1[2].y + t * bez2[1].y;
-}
-
-/**
- * divide the input curve at its halfway point
- */
-static inline void subdivideBezier(const CGPoint bez[4], CGPoint bez1[4], CGPoint bez2[4]){
-    subdivideBezierAtT(bez, bez1, bez2, .5);
-}
-
-/**
- * calculates the distance between two points
- */
-static inline CGFloat distanceBetween(CGPoint a, CGPoint b){
-    return hypotf( a.x - b.x, a.y - b.y );
-}
-
-/**
- * estimates the length along the curve of the
- * input bezier within the input acceptableError
- */
-CGFloat jotLengthOfBezier(const  CGPoint bez[4], CGFloat acceptableError){
-    CGFloat   polyLen = 0.0;
-    CGFloat   chordLen = distanceBetween (bez[0], bez[3]);
-    CGFloat   retLen, errLen;
-    NSUInteger n;
-    
-    for (n = 0; n < 3; ++n)
-        polyLen += distanceBetween (bez[n], bez[n + 1]);
-    
-    errLen = polyLen - chordLen;
-    
-    if (errLen > acceptableError) {
-        CGPoint left[4], right[4];
-        subdivideBezier (bez, left, right);
-        retLen = (jotLengthOfBezier (left, acceptableError)
-                  + jotLengthOfBezier (right, acceptableError));
-    } else {
-        retLen = 0.5 * (polyLen + chordLen);
-    }
-    
-    return retLen;
-}
-
-/**
- * will split the input bezier curve at the input length
- * within a given margin of error
- *
- * the two curves will exactly match the original curve
- */
-static CGFloat subdivideBezierAtLength (const CGPoint bez[4],
-                                        CGPoint bez1[4],
-                                        CGPoint bez2[4],
-                                        CGFloat length,
-                                        CGFloat acceptableError,
-                                        CGFloat* subBezierlengthCache){
-    CGFloat top = 1.0, bottom = 0.0;
-    CGFloat t, prevT;
-    
-    prevT = t = 0.5;
-    for (;;) {
-        CGFloat len1;
-        
-        subdivideBezierAtT (bez, bez1, bez2, t);
-
-        int lengthCacheIndex = (int)floorf(t*1000);
-        len1 = subBezierlengthCache[lengthCacheIndex];
-        if(!len1){
-            len1 = jotLengthOfBezier (bez1, 0.5 * acceptableError);
-            subBezierlengthCache[lengthCacheIndex] = len1;
-        }
-        
-        if (fabs (length - len1) < acceptableError){
-            return len1;
-        }
-        
-        if (length > len1) {
-            bottom = t;
-            t = 0.5 * (t + top);
-        } else if (length < len1) {
-            top = t;
-            t = 0.5 * (bottom + t);
-        }
-        
-        if (t == prevT){
-            subBezierlengthCache[lengthCacheIndex] = len1;
-            return len1;
-        }
-        
-        prevT = t;
-    }
-}
-
-
 
 #pragma mark - PlistSaving
 
